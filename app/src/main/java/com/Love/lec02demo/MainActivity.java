1f71a0e81d3f12eafa4c60ae308e9875137e96de
300310436 <lovepreetsinghibirdi@gmail.com>
2020-01-14 10:34:15 -0800

    Acticity 2

--- app/src/main/java/com/Love/lec02demo/MainActivity.java
+++ app/src/main/java/com/Love/lec02demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.Love.lec02demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent intent = new Intent(this, Recipe.class);
+        final Button show_Recipe = findViewById(R.id.button);
+        show_Recipe.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
     }
 }
