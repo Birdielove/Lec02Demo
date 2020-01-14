2ef6b38534f902abc203b5ab72f50b67c3100eb9
300310436 <lovepreetsinghibirdi@gmail.com>
2020-01-14 11:54:59 -0800

    Complete with Log

--- app/src/main/java/com/Love/lec02demo/MainActivity.java
+++ app/src/main/java/com/Love/lec02demo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Intent intent = new Intent(this, Recipe.class);
+        final Intent intent = new Intent( MainActivity.this, Recipe.class);
         final Button show_Recipe = findViewById(R.id.button);
         show_Recipe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+               Log.w("New", "Clicked on view recipe");
                 startActivity(intent);
             }
         });
